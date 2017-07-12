5ddac0066d0b9291857c169968a0b2ddd9b31b6e
Maurizio Pozzobon <mauriziopz@gmail.com>
2017-07-12 14:43:27 +0200

    Lab 6.1

---
+++ events.tac.local/Controllers/FeaturedEventController.cs
@@ -0,0 +1,31 @@
+﻿using events.tac.local.Models;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class FeaturedEventController : Controller
+    {
+        // GET: FeaturedEvent
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+
+        private FeaturedEvent CreateModel()
+        {
+            var item = RenderingContext.Current.Rendering.Item;
+            return new FeaturedEvent()
+            {
+                Heading = new HtmlString(FieldRenderer.Render(item, "ContentHeading")),
+                EventImage = new HtmlString(FieldRenderer.Render(item, "Event Image", "mw=400")),
+                Intro = new HtmlString(FieldRenderer.Render(item, "ContentIntro")),
+            };
+        }
+    }
+}
---
+++ events.tac.local/Models/FeaturedEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class FeaturedEvent
+    {
+        public FeaturedEvent()
+        {
+
+        }
+
+        public HtmlString Heading { get; set; }
+        public HtmlString Intro { get; set; }
+        public HtmlString EventImage { get; set; }
+    }
+}
