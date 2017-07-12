178c09d0deea0472b731952a3fcb5e311fec50e2
Maurizio Pozzobon <mauriziopz@gmail.com>
2017-07-12 12:14:19 +0200

    Lab 5.1

---
+++ events.tac.local/Controllers/EventIntroController.cs
@@ -0,0 +1,34 @@
+﻿using events.tac.local.Models;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class EventIntroController : Controller
+    {
+        // GET: EventIntro
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+        private static EventIntro CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            return new EventIntro()
+            {
+                Heading = new HtmlString(FieldRenderer.Render(item, "ContentHeading")),
+                EventImage = new HtmlString(FieldRenderer.Render(item, "Event Image", "mw=400")),
+                Highlights = new HtmlString(FieldRenderer.Render(item, "Highlights")),
+                Intro = new HtmlString(FieldRenderer.Render(item, "ContentIntro")),
+                StartDate = new HtmlString(FieldRenderer.Render(item, "Start Date")),
+                Duration = new HtmlString(FieldRenderer.Render(item, "Duration")),
+                Difficulty = new HtmlString(FieldRenderer.Render(item, "Difficulty Level")),
+            };
+        }
+    }
+}
---
+++ events.tac.local/Models/EventIntro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class EventIntro
+    {
+        public EventIntro()
+        {
+
+        }
+        public HtmlString Heading { get; set; }
+        public HtmlString Intro { get; set; }
+        public HtmlString Body { get; set; }
+        public HtmlString EventImage { get; set; }
+        public HtmlString Highlights { get; set; }
+        public HtmlString StartDate { get; set; }
+        public HtmlString Duration { get; set; }
+        public HtmlString Difficulty { get; set; }
+    }
+}
