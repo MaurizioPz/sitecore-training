1852879509f7cd525ca36ed8a2ea43b06539da66
Maurizio Pozzobon <mauriziopz@gmail.com>
2017-07-13 09:56:08 +0200

    Lab 7.2

---
+++ events.tac.local/Controllers/BreadcrumbController.cs
@@ -0,0 +1,36 @@
+﻿using events.tac.local.Models;
+using Sitecore;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class BreadcrumbController : Controller
+    {
+        // GET: Breadcrumb
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+
+        private IEnumerable<NavigationItem> CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            var home = Context.Database.GetItem(Context.Site.StartPath);
+            var breadcrumb = item.Axes.GetAncestors()
+                .Where(i => i.Axes.IsDescendantOf(home))
+                .Concat(new[] { item });
+            return breadcrumb.Select(b => new NavigationItem
+            {
+                Title = b.DisplayName,
+                URL = LinkManager.GetItemUrl(b),
+                Active = b.ID == item.ID
+            });
+        }
+    }
+}
---
+++ events.tac.local/Models/NavigationItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class NavigationItem
+    {
+        public NavigationItem()
+        {
+
+        }
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
