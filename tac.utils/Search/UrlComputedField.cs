f87784ac60aadc5110ef9543f47678d8a702eebb
Maurizio Pozzobon <mauriziopz@gmail.com>
2017-07-14 11:11:25 +0200

    Lab 11.3

---
+++ tac.utils/Search/UrlComputedField.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.ContentSearch.ComputedFields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.ContentSearch;
+using Sitecore.Links;
+
+namespace TAC.Utils.Search
+{
+    class UrlComputedField : AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            var indexableItem = indexable as SitecoreIndexableItem;
+            if (indexableItem == null)
+                return null;
+            var item = indexableItem.Item;
+            var options = LinkManager.GetDefaultUrlOptions();
+            options.SiteResolving = true;
+            return LinkManager.GetItemUrl(item, options);
+        }
+    }
+}
