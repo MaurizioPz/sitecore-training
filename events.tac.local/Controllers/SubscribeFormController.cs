e0825841e92b2b017a4692243e1e05bf8114d999
Maurizio Pozzobon <mauriziopz@gmail.com>
2017-07-14 10:54:52 +0200

    Lab 11.2

--- events.tac.local/Controllers/SubscribeFormController.cs
+++ events.tac.local/Controllers/SubscribeFormController.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Sitecore.Analytics;
+using Sitecore.Analytics.Model.Entities;
+using Sitecore.Analytics.Outcome.Model;
+using Sitecore.Analytics.Outcome.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TAC.Utils.Mvc;
+using Sitecore.Data;
 
 namespace events.tac.local.Controllers
 {
@@ -18,6 +23,17 @@ namespace events.tac.local.Controllers
         [ValidateFormHandler]
         public ActionResult Index(string email)
         {
+            Tracker.Current.Session.Identify(email);
+            var contact = Tracker.Current.Contact;
+            var emails = contact.GetFacet<IContactEmailAddresses>("Emails");
+            if (!emails.Entries.Contains("personal"))
+            {
+                emails.Preferred = "personal";
+                var personalEmail = emails.Entries.Create("personal");
+                personalEmail.SmtpAddress = email;
+            }
+            var outcome = new ContactOutcome(ID.NewID, new ID("{8DAFFB64-56BE-4A19-81E2-5CB158299E5E}"), new ID(contact.ContactId));
+            Tracker.Current.RegisterContactOutcome(outcome);
             return View("Confirmation");
         }
     }
